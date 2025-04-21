4fde23f757fe24b7b15917906ebfef78077348bf
ballboyDev <yswgood0329@gmail.com>
2025-04-21 13:28:23 +0900

    20250421

---
+++ ver-node2/_post/index.md
@@ -0,0 +1,21 @@
+---
+date: 99999999
+prev: 1
+next: 2
+---
+
+# 심심한 개발자의 취미생활
+- 심심한 개발자의 일상 생활을 끄적여보는 일기장 같은 공간 입니다.
+
+### 자취생 밥 만들어 먹기
+- 집에서 해먹어 보는 요리의 레시피 저장용 입니다.
+
+### 심심한 개발자의 취미 개발
+- 업무가 아닌 취미로 만들어보는 다양한 개발 놀이와 이 블로그에 대한 글을 작성하는 공간 입니다.
+
+## Connection
+[![Static Badge](https://img.shields.io/badge/Blog-222222?logo=githubpages&logoSize=auto&style=for-the-badge)](https://ballboydev.github.io/)
+
+![Static Badge](https://img.shields.io/badge/yswgood0329%40gmail.com-EA4335?style=for-the-badge&logo=gmail&logoColor=EA4335&label=gmail&labelColor=FFFFFF)
+
+[![Static Badge](https://img.shields.io/badge/%40ballboy.329-FFFFFF?style=for-the-badge&logo=instagram&logoColor=FFFFFF&label=INSTA&labelColor=E4405F)](https://www.instagram.com/ballboy.329)
--- ver-node2/app.js
+++ ver-node2/app.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const path = require('path')
 const { marked } = require('marked')
 const matter = require('gray-matter')
+const dayjs = require('dayjs')
+const { default: axios } = require('axios')
 
 const post = require('./_layout/post')
 
@@ -9,17 +11,20 @@ const env = process.env.NODE_ENV
 
 const utils = {
     path: {
-        // post: '_post',
-        index: '/Users/ballboy/workspace/project/static-site-generator/post',
-        post: '/Users/ballboy/workspace/project/static-site-generator/post',
+        index: '_post',
+        // index: '/Users/ballboy/workspace/project/static-site-generator/post',
+        post: '_post',
+        // post: '/Users/ballboy/workspace/project/static-site-generator/post',
         dist: '_dist',
         assets: '_assets',
         dev: `file://${__dirname}/_dist`,
-        build: 'https://lazydev500.github.io',
+        build: 'https://ballboydev.github.io',
     },
+    img: {},
     post: {},
     navi: [],
-    contents: []
+    contents: [],
+    posting: [`# Posting List (${dayjs().format("YYYY.MM.DD")})\n`, '||title|date|prev|next|url|', '|:-:|:--|:-:|:-:|:-:|:--|'],
 }
 
 const app = {
@@ -31,11 +36,10 @@ const app = {
         app.mkNavi();
         app.mkMainPage();
         app.mkPostPage();
-
-        console.log(utils.post)
+        app.finalWork();
     },
     init: () => {
-        console.log('##### [ app.init ] #####')
+        console.log('\n##### [ app.init ] #####')
 
         if (fs.existsSync(utils.path.dist)) {
             fs.rmSync(utils.path.dist, { recursive: true })
@@ -50,9 +54,20 @@ const app = {
         fs.copyFileSync(`${utils.path.assets}/skin.js`, `${utils.path.dist}/assets/skin.js`)
         fs.cpSync(`${utils.path.assets}/img/`, `${utils.path.dist}/assets/img/`, { recursive: true })
 
+        console.log('>> set Environment <<')
+        console.group('set Path')
+        console.log(`index: ${utils.path.index}`)
+        console.log(`post: ${utils.path.post}`)
+        console.log(`build: ${utils.path.build}`)
+        console.groupEnd()
+
+        console.group('set Image')
+        console.groupEnd()
+
+
     },
     mkJson: () => {
-        console.log('##### [ app.mkJson ] #####')
+        console.log('\n##### [ app.mkJson ] #####')
 
         const recursion = (root, fold = []) => {
             const temp = {}
@@ -61,39 +76,47 @@ const app = {
             const post = fs.readdirSync(root).filter((v) => { return ['_Common', 'index.md'].indexOf(v) < 0 })
 
             post.map((v) => {
-                const isDir = fs.statSync(`${root}/${v}`).isDirectory()
-                const mdFile = isDir ? {} : matter(fs.readFileSync(`${root}/${v}`, 'utf8').trim())
+                const isDir = fs.statSync(`${root}/${v}`).isDirectory();
 
-                const [title, index] = isDir ? path.basename(v, path.extname(v)).split('_') : [mdFile.data?.title, mdFile.data?.index || 0]
+                if (isDir) {
+                    const [title, index] = path.basename(v, path.extname(v)).split('_');
 
-                if (parseInt(index) !== 0) {
-                    const child = isDir ? recursion(`${root}/${v}`, [...fold, index]) : {}
-                    const count = Object.keys(child).reduce((a, b, i) => {
-                        const [type, index] = b.split('_')
-                        return a + (type === 'dir' ? child[b].count : (parseInt(index) === 0 ? 0 : 1))
-                    }, 0)
+                    if (parseInt(index) !== 0) {
+                        const child = recursion(`${root}/${v}`, [...fold, index]);
+                        const count = Object.keys(child).reduce((a, b) => {
+                            const [type, index] = b.split('_');
+                            return a + (type === 'dir' ? child[b].count : (parseInt(index) === 0 ? 0 : 1));
+                        }, 0);
 
-                    const item = {
-                        title: title,
-                        index: parseInt(index),
-                        ...(isDir ?
-                            {
-                                count: count,
-                                children: { ...child },
-                            } :
-                            {
-                                path: `${root}/${v}`,
-                                fold: fold,
-                                ...mdFile.data,
-                                content: mdFile.content
-                            }
-                        )
+                        const item = {
+                            title,
+                            file: '',
+                            index: parseInt(index),
+                            count,
+                            children: { ...child }
+                        };
+
+                        temp[`dir_${index}`] = item;
                     }
+                } else {
+                    const mdFile = matter(fs.readFileSync(`${root}/${v}`, 'utf8').trim());
+                    const title = mdFile.data?.title;
+                    const index = mdFile.data?.index || 0;
 
-                    temp[`${isDir ? `dir` : 'post'}_${index}`] = item
+                    if (parseInt(index) !== 0) {
+                        const item = {
+                            title,
+                            file: path.basename(v, path.extname(v)),
+                            index: parseInt(index),
+                            path: `${root}/${v}`,
+                            fold,
+                            date: mdFile.data?.date || '99999999',
+                            ...mdFile.data,
+                            content: mdFile.content
+                        };
 
-                    if (!isDir) {
-                        utils.contents.push(item)
+                        temp[`post_${index}`] = item;
+                        utils.contents.push(item);
                     }
                 }
 
@@ -108,16 +131,28 @@ const app = {
         fs.writeFileSync(`test/test.json`, JSON.stringify(utils.post))
     },
     mkNavi: () => {
-        console.log('##### [ app.mkNavi ] #####')
+        console.log('\n##### [ app.mkNavi ] #####')
 
         const tagList = []
 
         const recursion = (root) => {
             const item = Object.keys(root)
 
-            item.map((v) => {
-                const [type, num] = v.split('_')
+            item.sort((a, b) => {
+                const [aType, aIndex] = a.split('_')
+                const [bType, bIndex] = b.split('_')
 
+                if (aType === bType) {
+                    return parseInt(aIndex) - parseInt(bIndex)
+                } else {
+                    if (aType === 'post') {
+                        return -1
+                    } else {
+                        return 1
+                    }
+                }
+            }).map((v) => {
+                const [type, num] = v.split('_')
 
                 if (type === 'dir') {
                     tagList.push(`<li id="dt-${num}" onclick="foldNavi(${num})">${root[v].title} (${root[v].count})</li>`)
@@ -126,11 +161,9 @@ const app = {
                     tagList.push('</ul>')
                 } else {
                     tagList.push(`<a href="${utils.path[env]}/post/${num}.html">`)
-                    tagList.push(`<li id="p-${num}">${root[v].title}</li>`)
+                    tagList.push(`<li id="p-${num}">${root[v]?.title || root[v]?.file}</li>`)
                     tagList.push(`</a>`)
                 }
-
-
             })
 
             return tagList
@@ -140,7 +173,7 @@ const app = {
         utils.navi = tagList
     },
     mkMainPage: () => {
-        console.log('##### [ app.mkMainPage ] #####')
+        console.log('\n##### [ app.mkMainPage ] #####')
 
         const mdFile = matter(fs.readFileSync(`${utils.path.index}/index.md`, 'utf8').trim())
         const htmlFile = marked.parse(mdFile.content)
@@ -161,9 +194,11 @@ const app = {
 
     },
     mkPostPage: () => {
-        console.log('##### [ app.mkPostPage ] #####')
+        console.log('\n##### [ app.mkPostPage ] #####')
 
-        utils.contents.map((v) => {
+        utils.contents.sort((a, b) => {
+            return a.index - b.index
+        }).map((v) => {
 
             // ballboy / 포스팅 파일이 많아졌을때 성능/용량 이슈 발생 하지 않을지...?
             // const mdFile = matter(fs.readFileSync(v.path, 'utf8').trim())
@@ -182,7 +217,7 @@ const app = {
             // }
 
             // ballboy / 이미지 URL 변환 작업
-            const htmlFile = marked.parse(v.content).replaceAll(/(?<=")[^"]*(?=assets)/g, `${utils.path[env]}/`)
+            const htmlFile = marked.parse(v.content)//.replaceAll(/(?<=")[^"]*(?=assets)/g, `${utils.path[env]}/`)
 
             const metaData = {
                 url: utils.path[env],
@@ -197,9 +232,27 @@ const app = {
             const result = post.output(metaData)
 
             fs.writeFileSync(`${utils.path.dist}/post/${v.index}.html`, result)
-
-
         })
+    },
+    finalWork: async () => {
+        console.log('\n##### [ app.finalWork ] #####')
+
+        for (let i = 0; i < utils.contents.length; i++) {
+            const item = utils.contents[i]
+            let status = false
+            try {
+                let temp = await axios.get(`${utils.path.build}/post/${item.index}.html`)
+                status = temp.status === 200
+            } catch (err) {
+                console.log(`ERROR >> ${item.title}`)
+            }
+
+            const text = `|${item.index}|${item?.title || item?.file}|${item.date}|${item?.prev || ''}|${item?.next || ''}|${status ? `${utils.path.build}/post/${item.index}.html` : 'not yet'}|`
+            console.log(text)
+            utils.posting.push(text)
+        }
+
+        fs.writeFileSync(`posting.md`, utils.posting.join('\n'))
     }
 }
 
